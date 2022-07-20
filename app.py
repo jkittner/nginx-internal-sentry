54fc2390ec1a2e32b9dc8a30ba85746b05a6a1eb
Jonas Kittner <jonas.kittner@rub.de>
2022-07-20 16:47:13 +0200

    simplify even further

--- app.py
+++ app.py
@@ -60,5 +60,18 @@ def auth() -> Response:
     return 'OK'
 
 
+@app.route('/ajax_route', methods=['GET', 'POST'])
+def ajax() -> Response:
+    return {
+        'draw': 1,
+        'data': [
+            {'firstname': 'test', 'lastname': 'testing'},
+            {'firstname': 'test2', 'lastname': 'testing2'},
+        ],
+        'recordsTotal': 2,
+        'recordsFiltered': 2,
+    }
+
+
 if __name__ == '__main__':
     app.run()
